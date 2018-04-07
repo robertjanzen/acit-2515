deae6f93cd21615fc4652554a81601b1a4d655b4
Robert <robertjanzenbc@gmail.com>
2018-04-07 11:08:08 -0700

    Refactor transaction_db use model name

--- model/transaction_model.py
+++ model/transaction_model.py
@@ -1,20 +1,20 @@
 import csv
 import datetime
 
-class TransactionDB:
-    _DB_COLUMNS = ['date', 'uid', 'type', 'amount']
+class TransactionModel:
+    _TRANSACTION_COLUMNS = ['date', 'uid', 'type', 'amount']
 
-    def __init__(self, db_file):
-        self._db_file = db_file
-        self._db_content = []
+    def __init__(self, transaction_file):
+        self._transaction_file = transaction_file
+        self._transaction_content = []
 
         self.open_db_file()
 
     @property
-    def db_content(self):
-        return self._db_content
+    def transaction_content(self):
+        return self._transaction_content
 
-    def open_db_file(self):
+    def open_transaction_file(self):
         pass
 
     def create_new_entry(self, uid, type, amount, date=datetime.datetime.now()):
@@ -31,10 +31,10 @@ class TransactionDB:
     def save_to_file(self, uid):
         filename = uid+'transactions.txt'
         with open(filename, 'a', newline='') as csv_file:
-            fields = self._DB_COLUMNS
+            fields = self._TRANSACTION_COLUMNS
             writer = csv.DictWriter(csv_file, fieldnames=fields)
 
-            for entry in self.db_content:
+            for entry in self.transaction_content:
                 writer.writerow(entry)
 
 if __name__ == '__main__':
