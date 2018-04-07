643449c19f4b6009201025e37a9b20ed5fb4feeb
AHuang170 <32000451+AHuang170@users.noreply.github.com>
2018-04-06 21:17:35 -0700

    Changes_pt1

--- model/user_db.py
+++ model/user_db.py
@@ -1,3 +1,21 @@
+import csv
+
+
 class UserDB:
-    def __init__(self):
-        self.users = []
+    _DB_COLUMNS = ['uid', 'CardNum', 'hash'];
+    def __init__(self, db_file):
+        self._db_file = db_file
+        self._db_content = []
+        
+    def open_db_file(self):
+        with open(self._db_file, 'rb') as csvfile:
+            reader = csv.DictReader(csvfile)
+            
+            for row in reader:
+                new_dict = {}
+                for category in self._DB_COLUMNS:
+                    new_dict[category] = row[category]
+                    
+            self._db_content.push(new_dict)
+        print(self._db_content)
+        
