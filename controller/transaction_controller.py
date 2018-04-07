50f00d8c7b76c5928373ddeabe6c47af6cd82e9c
Robert <robertjanzenbc@gmail.com>
2018-04-06 20:32:49 -0700

    Add transaction controller states

--- controller/transaction_controller.py
+++ controller/transaction_controller.py
@@ -5,11 +5,33 @@ from datetime import timedelta
 
 class TransactionController(Observer):
 
-    def __init__(self, view, state_model, user_db):
+    def __init__(self, view, state_model, account_db):
         self.view = view
         self.state_db = state_model
         self.state_db.add_observer(self)
-        self.db = user_db
+        self.account_db = account_db
+
+    def update(self, publisher, **kwargs):
+        updated_data = kwargs.keys()
+
+        if 'entry' in updated_data:
+            pass
+
+        elif 'input' in updated_data:
+            pass
+
+        elif 'state' in updated_data:
+            new_state = kwargs['state']
+            if new_state == 'deposit':
+                pass
+            elif new_state == 'overview':
+                pass
+            elif new_state == 'withdraw':
+                pass
+            elif new_state == 'done':
+                pass
+            elif new_state == 'info':
+                pass
 
 if __name__ == '__main__':
     print('Transaction Controller')
