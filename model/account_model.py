be6f5f4669639757ffbac3506342bc96e1f47123
robertjanzen <robertjanzenbc@gmail.com>
2018-04-08 15:27:42 -0700

    Add comment header for account model

--- model/account_model.py
+++ model/account_model.py
@@ -1,3 +1,13 @@
+# account_model.py
+#
+# ATM MVC program
+#
+# Team alroda
+#
+# Aldrich Huang A01026502 2B
+# Robert Janzen A01029341 2B
+# David Xiao A00725026 2B
+
 import json
 from model.constants import *
 
